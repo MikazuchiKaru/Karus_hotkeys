c61193df75b01da101054c6ba99677cc857fc5b3
Ondřej Leitgeb <leitgon@student.uzlabina.com>
2025-02-07 22:18:46 +0100

    fixed console issue and added messagebox for when closed

--- hotkey/main.cpp
+++ hotkey/main.cpp
@@ -7,5 +7,6 @@ int main()
 {
     std::vector<macro> macros = parse("macro.txt");
     if (hotkeycheck(macros)) return 1;
+    MessageBoxA(NULL, "app closed successfully", "info", MB_OK);
     return 0;
 }
